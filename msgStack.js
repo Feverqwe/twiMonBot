3625a6945aefa2720b5cd8eb7282319e4bd5df11
Anton <anton9121@gmail.com>
2017-02-26 16:02:36 +0500

    1
    (cherry picked from commit c75683a)

--- msgStack.js
+++ msgStack.js
@@ -3,7 +3,7 @@
  */
 "use strict";
 var base = require('./base');
-var debug = require('debug')('app:MsgStack');
+var debug = require('debug')('app:msgStack');
 var Promise = require('bluebird');
 
 var MsgStack = function (options) {
