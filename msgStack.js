45f2e8feabab7cec77d1b7a8958ee97851327735
Anton <anton9121@gmail.com>
2017-03-26 09:39:52 +0500

    1

--- msgStack.js
+++ msgStack.js
@@ -4,7 +4,7 @@
 "use strict";
 var base = require('./base');
 var debug = require('debug')('app:msgStack');
-var debugLog = require('debug')('app:msgSender:log');
+var debugLog = require('debug')('app:msgStack:log');
 debugLog.log = console.log.bind(console);
 var Promise = require('bluebird');
 
