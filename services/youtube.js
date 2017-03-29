0af6623e93dcf724b334711beffae8d63be35e7e
Anton <anton9121@gmail.com>
2017-03-29 21:52:09 +0500

    1

--- services/youtube.js
+++ services/youtube.js
@@ -365,10 +365,6 @@ Youtube.prototype.requestChannelIdByUsername = function(url) {
  * @returns {Promise.<String>}
  */
 Youtube.prototype.getChannelIdByUrl = function (url) {
-    if (/^UC/.test(url)) {
-        return Promise.resolve(url);
-    }
-
     var channelId = '';
     [
         /youtube\.com\/(?:#\/)?channel\/([\w\-]+)/i
@@ -381,10 +377,14 @@ Youtube.prototype.getChannelIdByUrl = function (url) {
     });
 
     if (!channelId) {
-        return Promise.reject(new CustomError('It is not channel url!'));
-    } else {
-        return Promise.resolve(channelId);
+        channelId = url;
     }
+
+    if (!/^UC/.test(channelId)) {
+        return Promise.reject(new CustomError('It is not channel url!'));
+    }
+
+    return Promise.resolve(channelId);
 };
 
 /**
