c18cd7e842b50286aa4a0beeebc0b6fdbdf368e0
Anton <leonardspbox@gmail.com>
2019-02-13 21:19:16 +0500

    proxyList check

--- proxyList.js
+++ proxyList.js
@@ -1,6 +1,8 @@
 const debug = require('debug')('app:proxyList');
 const tunnel = require('tunnel');
 const got = require('got');
+const promiseLimit = require('promise-limit');
+const {arrToParts} = require('./base');
 
 const oppositeType = {
     online: 'offline',
@@ -54,14 +56,21 @@ class ProxyList {
 
     check() {
         if (this.checkPromise) return this.checkPromise;
+        // debug('checking...');
+
+        const limit = promiseLimit(8);
 
         return this.checkPromise = Promise.resolve().then(() => {
             const next = (agents, index) => {
                 const agent = agents[index];
                 if (!agent) return;
+                // debug('check', agentToString(agent));
 
                 const url = this.testRequst.url;
-                const options = Object.assign({agent}, this.testRequst.options);
+                const options = Object.assign({
+                    agent,
+                    timeout: 5 * 1000
+                }, this.testRequst.options);
                 const startTime = Date.now();
                 return got(url, options).then(() => {
                     agent._latency = Date.now() - startTime;
@@ -74,25 +83,23 @@ class ProxyList {
                     return next(agents, index + 1);
                 });
             };
-            return Promise.all([
-                next(this.online, 0),
-                next(this.offline, 0),
-            ]);
+
+            const agents = [].concat(this.online, this.offline);
+            return Promise.all(arrToParts(agents, Math.trunc(agents.length / 8)).map(arr => {
+                return limit(() => next(arr, 0));
+            }));
         }).then(() => {
             this.online.sort((a, b) => {
                 return a._latency > b._latency ? 1 : -1;
             });
 
-            /*
-            const agentToString = (agent) => {
-                return `${agent.proxyOptions.host}:${agent.proxyOptions.port}`;
-            };
-            const online = this.online.map(agentToString);
-            const offline = this.offline.map(agentToString);
-            debug(`Check state: %s/%s`, online.length, offline.length);
-            debug(`Online: %j`, online);
-            debug(`Offline: %j`, offline);
-            */
+            if (false) {
+                const online = this.online.map(agentToString);
+                const offline = this.offline.map(agentToString);
+                debug(`Check state: %s/%s`, online.length, offline.length);
+                debug(`Online: %j`, online);
+                debug(`Offline: %j`, offline);
+            }
 
             this.checkPromise = null;
         });
@@ -143,6 +150,10 @@ class ProxyList {
 
 const isProxyError = (err) => {
     return /tunneling socket could not be established/.test(err.message);
+};
+
+const agentToString = (agent) => {
+    return `${agent.proxyOptions.host}:${agent.proxyOptions.port}`;
 };
 
 module.exports = ProxyList;
