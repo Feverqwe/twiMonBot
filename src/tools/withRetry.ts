b2bad8c18a57a2b3baadbd14bd164925b31e0821
Anton <leonardspbox@gmail.com>
2019-07-21 00:19:33 +0500

    1

--- src/tools/withRetry.ts
+++ src/tools/withRetry.ts
@@ -1,4 +1,4 @@
-async function withRetry<T>(params: {count: number, timeout: number|null}, callback: () => T, ...errorHandlers:Function[]):Promise<T> {
+async function withRetry<T>(params: {count: number, timeout?: number}, callback: () => T, ...errorHandlers:Function[]):Promise<T> {
   if (typeof params === 'function') {
     errorHandlers.unshift(callback);
     callback = params;
