40a3dafe8537c19adea4cae2ab877e8e62401dbd
Anton <leonardspbox@gmail.com>
2019-07-21 00:16:37 +0500

    ts

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import babel from 'rollup-plugin-babel';
+import typescript from 'rollup-plugin-typescript';
 
 export default {
   input: ['./src/main.js', './src/testProxies.js'],
@@ -7,6 +8,7 @@ export default {
     format: 'cjs'
   },
   plugins: [
+    typescript(),
     babel({
       plugins: [
         ['@babel/plugin-proposal-class-properties', { "loose": true }]
