9759537c6afeb6e5fd92325e239ff4b2a90e9b24
Anton <leonardspbox@gmail.com>
2019-07-23 00:06:40 +0500

    1

--- src/tools/ensureMap.ts
+++ src/tools/ensureMap.ts
@@ -1,4 +1,4 @@
-const ensureMap = <A, B>(map: Map<A, B>, id: A, defaultValue: B) => {
+const ensureMap = <A, B>(map: Map<A, B>, id: A, defaultValue: B): B => {
   let value = map.get(id);
   if (!value) {
     map.set(id, value = defaultValue);
