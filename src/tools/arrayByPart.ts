c7ead341f7e6ddeac431dba616bb594619583313
Anton <leonardspbox@gmail.com>
2019-07-23 11:11:10 +0500

    1

--- src/tools/arrayByPart.ts
+++ src/tools/arrayByPart.ts
@@ -1,12 +1,11 @@
 const arrayByPart = <T>(array: T[], len: number): T[][] => {
-  const size = Math.ceil(array.length / len);
+  const size = !len ? 0 : Math.ceil(array.length / len);
   const parts = new Array(size);
   let offset = 0;
   let index = 0;
   while (index < size) {
-    const part = array.slice(offset, offset + len);
+    parts[index++] = array.slice(offset, offset + len);
     offset += len;
-    parts[index++] = part;
   }
   return parts;
 };
