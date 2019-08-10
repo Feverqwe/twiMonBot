cd2f41ec350f286ac11a1822c48ad04edf0ce433
Anton <leonardspbox@gmail.com>
2019-08-10 07:55:23 +0500

    rm legacy

--- legacy/errorWithCode.js
+++
@@ -1,9 +0,0 @@
-class ErrorWithCode extends Error {
-    constructor(message, code) {
-        super(message);
-
-        this.code = code;
-    }
-}
-
-module.exports = ErrorWithCode;
--- legacy/testProxy.js
+++
@@ -1,4 +0,0 @@
-const ProxyList = require('./proxyList');
-const base = require('./base');
-const config = base.loadConfig();
-const proxyList = new ProxyList({config});
