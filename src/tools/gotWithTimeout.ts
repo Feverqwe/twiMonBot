d65ca01c36b51e456892247ae0863734c2d536b9
Anton <leonardspbox@gmail.com>
2019-08-23 17:37:26 +0500

    fix

--- src/tools/gotWithTimeout.ts
+++ src/tools/gotWithTimeout.ts
@@ -1,15 +1,23 @@
 const timeout = 60 * 1001;
 
-const gotWithTimeout = require('got').extend({
-  timeout: timeout,
-  hooks: {
-    beforeError: [(err: any) => {
-      if (err.name === 'TimeoutError' && err.gotOptions.timeout === timeout) {
-        err.name = 'LockTimeoutError';
-      }
-      return err;
-    }]
-  }
+const aliases = ['get', 'post', 'put', 'patch', 'head', 'delete'];
+
+const got = require('got').extend({
+  timeout: timeout
 });
+
+const gotWithTimeout = (url: string, options: any) => {
+  return got(url, options).catch((err: any) => {
+    if (err.name === 'TimeoutError' && err.gotOptions.timeout === timeout) {
+      err.name = 'LockTimeoutError';
+    }
+    throw err;
+  });
+};
+
+for (const method of aliases) {
+  // @ts-ignore
+  gotWithTimeout[method] = (url, options) => gotWithTimeout(url, {...options, method});
+}
 
 export default gotWithTimeout;
