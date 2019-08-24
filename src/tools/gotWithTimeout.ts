e17f319c9aac66621d0e7e47a87f4c595c61aea1
Anton <leonardspbox@gmail.com>
2019-08-24 08:20:55 +0500

    gotWithTimeout

--- src/tools/gotWithTimeout.ts
+++ src/tools/gotWithTimeout.ts
@@ -1,18 +1,25 @@
-const timeout = 60 * 1001;
+import promiseFinally from "./promiseFinally";
+import ErrorWithCode from "./errorWithCode";
 
 const aliases = ['get', 'post', 'put', 'patch', 'head', 'delete'];
 
-const got = require('got').extend({
-  timeout: timeout
-});
+const got = require('got');
+
+interface gotFn {
+  (url: string, options?: object): Promise<any>,
+}
+
+interface gotAliases {
+  get: gotFn,
+  post: gotFn,
+  put: gotFn,
+  patch: gotFn,
+  head: gotFn,
+  delete: gotFn,
+}
 
 const gotWithTimeout = (url: string, options: any) => {
-  return got(url, options).catch((err: any) => {
-    if (err.name === 'TimeoutError' && err.gotOptions && err.gotOptions.gotTimeout && err.gotOptions.gotTimeout.request === timeout) {
-      err.name = 'LockTimeoutError';
-    }
-    throw err;
-  });
+  return gotLockTimeout(got(url, options));
 };
 
 for (const method of aliases) {
@@ -20,4 +27,20 @@ for (const method of aliases) {
   gotWithTimeout[method] = (url, options) => gotWithTimeout(url, {...options, method});
 }
 
-export default gotWithTimeout;
+function gotLockTimeout(request: Promise<any> & {cancel: () => void}): Promise<any> {
+  let lockTimeoutFired = false;
+  const timeout = setTimeout(() => {
+    lockTimeoutFired = true;
+    request.cancel();
+  }, 60 * 1000);
+  return request.then(...promiseFinally(() => {
+    clearTimeout(timeout);
+  })).catch((err: any) => {
+    if (err.name === 'CancelError' && lockTimeoutFired) {
+      throw new ErrorWithCode('Lock timeout fired', 'LockTimeoutError');
+    }
+    throw err;
+  });
+}
+
+export default gotWithTimeout as gotFn & gotAliases;
