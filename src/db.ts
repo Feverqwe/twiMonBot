f05430cada036fa35fca4871e1a0b2e193b5da5d
Anton <leonardspbox@gmail.com>
2019-09-11 12:57:48 +0500

    upd.. null in bottom

--- src/db.ts
+++ src/db.ts
@@ -621,7 +621,7 @@ class Db {
         syncTimeoutExpiresAt: {[Op.lt]: new Date()},
         lastSyncAt: {[Op.lt]: date},
       },
-      order: ['lastStreamAt', 'lastSyncAt'],
+      order: Sequelize.literal(`-lastStreamAt DESC, lastSyncAt`),
       limit: limit,
     });
   }
