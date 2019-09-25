01adee5acc7dd95ac1e59cfef89af21ffcac21e9
Anton <leonardspbox@gmail.com>
2019-09-25 09:40:37 +0500

    1

--- rollup.config.js
+++ rollup.config.js
@@ -1,41 +1,18 @@
 import babel from 'rollup-plugin-babel';
-import resolve from "rollup-plugin-node-resolve";
-
-const extensions = ['.js', '.ts'];
+import typescript from 'rollup-plugin-typescript';
 
 export default {
-  input: './src/main.ts', // ['./src/main.ts', './src/testProxies.js'],
+  input: ['./src/main.ts', './src/testProxies.js'],
   output: {
     dir: './dist',
     format: 'cjs'
   },
   plugins: [
-    resolve({
-      modulesOnly: true,
-      extensions,
-    }),
+    typescript(),
     babel({
-      presets: [
-        ['@babel/preset-env', {
-          useBuiltIns: 'usage',
-          corejs: {
-            version: 3,
-            proposals: true
-          },
-          targets: {
-            node: 'current'
-          },
-          include: [
-            'es.promise.finally',
-            'esnext.promise.try',
-          ]
-        }]
-      ],
       plugins: [
-        '@babel/plugin-transform-typescript',
         ['@babel/plugin-proposal-class-properties', { "loose": true }]
-      ],
-      extensions,
-    }),
+      ]
+    })
   ]
 };
