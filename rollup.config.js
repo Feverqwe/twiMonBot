83b91f6ef8d95d6a8f882ec986c72f06d81b9a63
Anton <leonardspbox@gmail.com>
2021-10-26 08:52:54 +0500

    rm rollup-plugin-typescript

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import typescript from 'rollup-plugin-typescript';
+import typescript from '@rollup/plugin-typescript';
 import run from '@rollup/plugin-run';
 
 const isWatch = process.argv.includes('-w');
