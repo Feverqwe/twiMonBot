bf37c899ca18e8b65e1fbe3bd6084496011295e9
Anton <leonardspbox@gmail.com>
2022-04-24 11:42:53 +0500

    pubsub change rate limit

--- src/tools/expressPubSub.ts
+++ src/tools/expressPubSub.ts
@@ -7,7 +7,7 @@ import RateLimit2 from "./rateLimit2";
 import assertType from "./assertType";
 import getTimeGraph from "./timeGrapth";
 
-const rateLimit = new RateLimit2(250);
+const rateLimit = new RateLimit2(5);
 
 const debug = require('debug')('app:ExpressPubSub');
 const timeGraph = getTimeGraph();
