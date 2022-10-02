3c57ec1921ed7d32a6ab5e132356473dc5bfacb5
Anton <leonardspbox@gmail.com>
2022-10-02 12:05:32 +0500

    fix

--- src/locale.ts
+++ src/locale.ts
@@ -3,7 +3,8 @@ import en from "./locale/en";
 class Locale {
   getMessage(messageName: keyof typeof languages[keyof typeof languages], variables?: Record<string, number | string>) {
     const message = languages.en[messageName];
-    return message.replace(/\{([^}]+)}/, (variable) => {
+    return message.replace(/\{([^}]+)}/g, (template) => {
+      const variable = template.slice(1, -1);
       return String(variables && variables[variable] || variable);
     });
   }
