0a0a13ac8e6422478ca3c65e6cbd7334ae713228
Anton <Feverqwe@users.noreply.github.com>
2025-01-03 00:04:35 +0300

    rm unused var

--- src/services/kick.ts
+++ src/services/kick.ts
@@ -8,8 +8,6 @@ import Main from '../main';
 
 const debug = getDebug('app:kick');
 
-const UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0';
-
 const StreamStruct = s.object({
   data: s.nullable(
     s.object({
